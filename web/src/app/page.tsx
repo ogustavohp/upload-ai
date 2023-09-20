0162c07fff0f1283b0d67869072da72c30c57f8e
GusttavoHP <107812402+GusttavoHP@users.noreply.github.com>
2023-09-20 19:48:50 -0300

    att `home page` web

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,18 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
-  return <div>opa</div>
+  return (
+    <div>
+      <div className="flex items-center justify-between border-b px-6 py-3">
+        <h1 className="text-xl font-bold">upload.ai</h1>
+
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            Desenvolvido por @ogustavohp
+          </span>
+          <Button variant="outline">Github</Button>
+        </div>
+      </div>
+    </div>
+  )
 }
