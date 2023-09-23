d21d4acf1b25deb6c7296f67dc9b35921a84dca2
GusttavoHP <107812402+GusttavoHP@users.noreply.github.com>
2023-09-23 12:54:27 -0300

    create `script dev` server

---
+++ server/src/server.ts
@@ -0,0 +1,13 @@
+import { fastify } from 'fastify'
+
+const app = fastify()
+
+app.get('/', () => {
+  return 'Hello World'
+})
+
+app.listen({
+  port: 3333
+}).then(() => {
+  console.log('HTTP Server Running! http://localhost:3333/')
+})
