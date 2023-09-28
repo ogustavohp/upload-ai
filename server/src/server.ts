6391af8e73f0d278d76897ce68f74f4e34ae0b5b
GusttavoHP <107812402+GusttavoHP@users.noreply.github.com>
2023-09-28 20:06:05 -0300

    create `create-transcription` route server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,13 @@
 import { fastify } from 'fastify'
 import { getAllPromptsRoute } from './routes/get-all-prompts'
 import { uploadVideoRoutes } from './routes/upload-video'
+import { createTranscriptionRoute } from './routes/create-transcription'
 
 const app = fastify()
 
 app.register(getAllPromptsRoute)
 app.register(uploadVideoRoutes)
+app.register(createTranscriptionRoute)
 
 app
   .listen({
