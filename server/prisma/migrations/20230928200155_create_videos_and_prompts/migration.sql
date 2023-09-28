f84f3499d2e08b006db602bd2777a580d42e7b35
GusttavoHP <107812402+GusttavoHP@users.noreply.github.com>
2023-09-28 17:08:11 -0300

    att: `prisma schema` server

--- server/prisma/migrations/20230928200155_create_videos_and_prompts/migration.sql
+++ server/prisma/migrations/20230928200155_create_videos_and_prompts/migration.sql
@@ -3,7 +3,7 @@ CREATE TABLE "Video" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "name" TEXT NOT NULL,
     "path" TEXT NOT NULL,
-    "trasncription" TEXT NOT NULL,
+    "transcription" TEXT,
     "createAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
