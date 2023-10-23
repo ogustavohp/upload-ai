57eab9bbaf938a16345b1259481e335bf85ec558
GusttavoHP <107812402+GusttavoHP@users.noreply.github.com>
2023-10-23 19:06:56 -0300

    update ai model for `completion`

--- server/src/routes/generate-ai-completion.ts
+++ server/src/routes/generate-ai-completion.ts
@@ -31,7 +31,7 @@ export async function generateAiCompletionRoute(app: FastifyInstance) {
     )
 
     const response = await openai.chat.completions.create({
-      model: 'gpt-3.5-turbo',
+      model: 'gpt-3.5-turbo-16k',
       temperature,
       messages: [{ role: 'user', content: promptMessage }],
     })
