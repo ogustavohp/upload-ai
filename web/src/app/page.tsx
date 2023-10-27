be77ff89a4670ffaee6e27eabfc2cee49d3ae132
GusttavoHP <107812402+GusttavoHP@users.noreply.github.com>
2023-10-27 20:35:59 -0300

    Refactor:Transformed video input in a component

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -10,7 +10,8 @@ import {
 import { Separator } from '@/components/ui/separator'
 import { Slider } from '@/components/ui/slider'
 import { Textarea } from '@/components/ui/textarea'
-import { Github, FileVideo, Upload, Wand2 } from 'lucide-react'
+import VideoInputForm from '@/components/videoInputForm'
+import { Github, Wand2 } from 'lucide-react'
 
 export default function Home() {
   return (
@@ -54,40 +55,7 @@ export default function Home() {
         </div>
 
         <aside className="w-80 space-y-6">
-          <form className="space-y-6" action="">
-            <label
-              htmlFor="video"
-              className="flex aspect-video cursor-pointer flex-col items-center justify-center gap-2 rounded-md border border-dashed text-sm text-muted-foreground hover:bg-primary/10"
-            >
-              <FileVideo className="h-4 w-4" />
-              Selecione um vídeo
-            </label>
-            <input
-              type="file"
-              name="video"
-              id="video"
-              accept="video/mp4"
-              className="sr-only"
-            />
-
-            <Separator />
-
-            <div className="space-y-2">
-              <Label htmlFor="transcription_prompt">
-                Prompt de transcrição
-              </Label>
-              <Textarea
-                id="transcription_prompt"
-                className="h-20 resize-none leading-relaxed"
-                placeholder="Inclua palavras-chave mencionadas no vídeo separadas por vírgula (,)"
-              />
-            </div>
-
-            <Button type="submit" className="w-full">
-              Carregar vídeo
-              <Upload className="ml-2 h-4 w-4" />
-            </Button>
-          </form>
+          <VideoInputForm />
 
           <form className="space-y-6">
             <div className="space-y-2">
