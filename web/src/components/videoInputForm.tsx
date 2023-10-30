8b4d92759f49d157e069d6facb4e6bf5f5117538
GusttavoHP <107812402+GusttavoHP@users.noreply.github.com>
2023-10-30 06:14:38 -0300

    create previewVideo

--- web/src/components/videoInputForm.tsx
+++ web/src/components/videoInputForm.tsx
@@ -1,19 +1,51 @@
+'use client'
 import { FileVideo, Upload } from 'lucide-react'
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 import { Button } from '@/components/ui/button'
 import { Textarea } from '@/components/ui/textarea'
 import { Separator } from '@/components/ui/separator'
 import { Label } from '@/components/ui/label'
 
 export default function VideoInputForm() {
+  const [videoFile, setVideoFile] = useState<File | null>(null)
+
+  function handleFileSelected(e: React.ChangeEvent<HTMLInputElement>) {
+    const { files } = e.currentTarget
+
+    if (!files) {
+      return
+    }
+
+    const selectedFile = files[0]
+
+    setVideoFile(selectedFile)
+  }
+
+  const previewURL = useMemo(() => {
+    if (!videoFile) {
+      return null
+    }
+    return URL.createObjectURL(videoFile)
+  }, [videoFile])
+
   return (
     <form className="space-y-6" action="">
       <label
         htmlFor="video"
-        className="flex aspect-video cursor-pointer flex-col items-center justify-center gap-2 rounded-md border border-dashed text-sm text-muted-foreground hover:bg-primary/10"
+        className="relative flex aspect-video cursor-pointer flex-col items-center justify-center gap-2 rounded-md border border-dashed text-sm text-muted-foreground hover:bg-primary/10"
       >
-        <FileVideo className="h-4 w-4" />
-        Selecione um vídeo
+        {previewURL ? (
+          <video
+            src={previewURL}
+            controls={false}
+            className="pointer-events-none absolute inset-0"
+          />
+        ) : (
+          <>
+            <FileVideo className="h-4 w-4" />
+            Selecione um vídeo
+          </>
+        )}
       </label>
       <input
         type="file"
@@ -21,6 +53,7 @@ export default function VideoInputForm() {
         id="video"
         accept="video/mp4"
         className="sr-only"
+        onChange={handleFileSelected}
       />
 
       <Separator />
