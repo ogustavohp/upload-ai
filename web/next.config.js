82f066dc12958b9315e88ddd14a878f2fe4a358f
GusttavoHP <107812402+GusttavoHP@users.noreply.github.com>
2023-10-30 18:52:38 -0300

    commented webpack next.config

--- web/next.config.js
+++ web/next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // webpack: (config) => {
+  //   config.module.rules.push({
+  //     test: /ffmpeg\.wasm$/,
+  //     type: 'javascript/auto',
+  //   })
+  //   return config
+  // },
+}
 
 module.exports = nextConfig
